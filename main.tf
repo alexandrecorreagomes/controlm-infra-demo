0a6116b9fe71c0206b3f56c498617796b9186545
alexandrecorreagomes <38055390+alexandrecorreagomes@users.noreply.github.com>
2026-08-26 15:17:20 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -6,47 +6,6 @@ terraform {
     }
   }
 }
-
-# Conecta ao motor do Docker local da VPS (onde o agente está rodando)
 provider "docker" {
   host = "unix:///var/run/docker.sock"
-}
-
-# Cria uma rede isolada para o laboratório
-resource "docker_network" "lab_net" {
-  name = "controlm_net"
-}
-
-# 1. Nó de Aplicação (Target Node que o Ansible vai configurar)
-resource "docker_container" "node_app" {
-  name  = "node_app_01"
-  image = "nginx:alpine"
-  networks_advanced {
-    name = docker_network.lab_net.name
-  }
-}
-
-# 2. Servidor Ansible (Control Node)
-resource "docker_container" "ansible_master" {
-  name  = "ansible_master"
-  image = "willhallonline/ansible:latest"
-  
-  # Mantém o contêiner rodando em background aguardando comandos do Control-M
-  command = ["tail", "-f", "/dev/null"]
-
-  # Mapeia o socket do Docker para conexão direta com os nodes
-  volumes {
-    host_path      = "/var/run/docker.sock"
-    container_path = "/var/run/docker.sock"
-  }
-  
-  # Mapeia a pasta local da sua VPS onde colocaremos o Playbook depois
-  volumes {
-    host_path      = "/opt/playbooks"
-    container_path = "/playbooks"
-  }
-
-  networks_advanced {
-    name = docker_network.lab_net.name
-  }
 }
